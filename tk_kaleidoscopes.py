b137dc74431414a003fe6b130507665a8d6acef3
Alexey <zakharchenko_240@mail.ru>
2021-09-05 18:41:22 +0300

    add_tk_kaleidoscopes

---
+++ tk_kaleidoscopes.py
@@ -0,0 +1,18 @@
+"""You sell souvenir kaleidoscopes at a gift shop, and if a customer buys more than one, they get a 10% discount on all of them!
+Given the total number of kaleidoscopes that a customer buys, let them know what their total will be. 
+Tax is 7%. All of your kaleidoscopes cost the same amount, 5.00.
+
+Task: 
+Take the number of kaleidoscopes that a customer buys and output their total cost including tax and any discounts.
+
+Input Format: 
+An integer value that represents the number of kaleidoscopes that a customer orders.
+
+Output Format: 
+A number that represents the total purchase price to two decimal places.
+
+Sample Input: 
+4
+
+Sample Output: 
+19.26"""
